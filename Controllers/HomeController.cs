e32193a0fca9a6e3b66ae634f48050c74c2e96c5
Morgan Drake <mdrake@chef.io>
2017-08-26 17:20:24 +0000

    successfully changed files to match tutorial, woo!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using asp_dot_noob.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
-namespace asp_dot_noob.Controllers
+namespace CharacterSheetApp.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      var name = "Big Edna";
+      return View("Index",name);
     }
+  }
 }
