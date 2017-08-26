08f57342d7a80216a9f22b964ad5fe6be2ea42c4
Morgan Drake <mdrake@chef.io>
2017-08-26 14:11:21 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asp_dot_noob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
